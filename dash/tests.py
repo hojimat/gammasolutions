8810c18936b032d77d0796917bf8171786ec891e
ravshansk <rsk@ravshansk.com>
2022-09-09 00:18:56 +0200

    wrote my first test ever

--- dash/tests.py
+++ dash/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class ModelTest(TestCase):
+    def setUp(self):
+        self.driver = Driver.objects.create(
+            first_name = "Ramona",
+            last_name = "Flowers",
+            birth_date = "1991-01-02",
+            gender = "F",
+            mc = "1234567",
+            g_rate = 0.3,
+            active = True)
+    
+    def test_driver(self):
+        ''' Testing the Driver model '''
+        ramona = Driver.objects.get(pk=self.driver.pk)
+        self.assertEqual(str(ramona), "Ramona Flowers")
